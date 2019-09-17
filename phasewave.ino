f7865254f9ff3956315640c1ebd6129309b826cc
Joakin Ugalde <jcugaldecas@gmail.com>
2019-09-17 00:42:04 -0300

    Add files via upload

--- phasewave.ino
+++ phasewave.ino
@@ -1,8 +1,8 @@
 #define ndev 12 // numers of devices, like LEDs
 #define nsteps 12 // number of steps in the primitive wave
 #define scale 1 // enlargement of the wave e.g if the wave is {1,0} and the scale is 2 it becomes {1,1,0,0}
-#define dt 80 // time between steps in miliseconds
-#define phaseshift 1 // step difference between modules
+#define dt 500// time between steps in miliseconds
+#define phaseshift 2 // step difference between modules
 
 #define clrbit(reg,bit) (reg & ~(1<<bit)) // to turn off a pin
 #define setbit(reg,bit) (reg | (1<<bit)) // to turn on a pin
@@ -10,12 +10,10 @@
 byte _pins[ndev] = {2, 3 , 4, 5, 6, 7, 13, 12, 11, 10, 9, 8};
 
 int tsteps = scale * nsteps; // final number of steps
-byte wave[nsteps] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // primite wave
+byte wave[nsteps] = {1, 1, 0,0,0,0,0,0,0,0,0,0}; // primite wave
 byte wave2[scale * nsteps]; // final wave
 byte _step = 0; // step index
 volatile byte* port_config[] = { &PORTD, &PORTB, &PORTC};
-byte portindex;
-byte cons;
 
 void generatewave() { // increases the resolution of the primitive wave by the scale factor
   for (int i = 0; i < tsteps; i++) {
@@ -23,25 +21,9 @@ void generatewave() { // increases the resolution of the primitive wave by the s
   }
 }
 void assign(byte pin, byte value) {
-  if (pin < 8) {
-    portindex = 0;
-    cons = 0;
-  }
-  else if (pin < 14) {
-    portindex = 1;
-    cons = 8;
-  }
-  else {
-    portindex = 2;
-    cons = 14;
-  }
-
-  if (0 == value) {
-    *port_config[portindex] = clrbit(*port_config[portindex], pin - cons);
-  }
-  else {
-    *port_config[portindex] = setbit(*port_config[portindex], pin - cons);
-  }
+  byte division = pin/8;
+  byte rest = pin % 8;
+  *port_config[division] = value ? setbit(*port_config[division], rest) : clrbit(*port_config[division], rest);
 }
 
 void setup() {
@@ -56,11 +38,8 @@ void loop() {
 
   actuate();
   delay(dt);
-  _step = _step + 1;
-  if (_step == tsteps) {
-    _step = 0;
+  _step = (_step + 1)%tsteps;
 
-  }
 
 }
 
--- phasewave2.ino
+++ phasewave2.ino
@@ -3,23 +3,21 @@
 #define scale 1 // enlargement of the wave e.g if the wave is {1,0} and the scale is 2 it becomes {1,1,0,0}
 #define dt 200 // time between steps in miliseconds
 #define phaseshifta 1 // step difference between modules
-#define phaseshiftb 2 // step difference between modules
+#define phaseshiftb 1 // step difference between modules
 
 #define clrbit(reg,bit) (reg & ~(1<<bit)) // to turn off a pin
 #define setbit(reg,bit) (reg | (1<<bit)) // to turn on a pin
 
 byte _pinsa[ndev] = {2, 3 , 4, 5, 6, 7};
-byte _pinsb[ndev] = {8, 9, 10, 11, 12, 13};
+byte _pinsb[ndev] = {13, 12, 11, 10, 9, 8};
 
 int tsteps = scale * nsteps; // final number of steps
 byte wave1a[nsteps] = {1, 0, 0, 0, 0, 0}; // primite wave
 byte wave2a[scale * nsteps]; // final wave
-byte wave1b[nsteps] = {1, 1, 0, 0, 0, 0}; // primite wave
+byte wave1b[nsteps] = {1, 1, 0, 0, 0 , 0}; // primite wave
 byte wave2b[scale * nsteps]; // final wave
 byte _step = 0; // step index
 volatile byte* port_config[] = { &PORTD, &PORTB, &PORTC};
-byte portindex;
-byte cons;
 
 void generatewave() { // increases the resolution of the primitive wave by the scale factor
   for (int i = 0; i < tsteps; i++) {
@@ -29,25 +27,9 @@ void generatewave() { // increases the resolution of the primitive wave by the s
 }
 
 void assign(byte pin, byte value) {
-  if (pin < 8) {
-    portindex = 0;
-    cons = 0;
-  }
-  else if (pin < 14) {
-    portindex = 1;
-    cons = 8;
-  }
-  else {
-    portindex = 2;
-    cons = 14;
-  }
-
-  if (0 == value) {
-    *port_config[portindex] = clrbit(*port_config[portindex], pin - cons);
-  }
-  else {
-    *port_config[portindex] = setbit(*port_config[portindex], pin - cons);
-  }
+  byte division = pin / 8;
+  byte rest = pin % 8;
+  *port_config[division] = value ? setbit(*port_config[division], rest) : clrbit(*port_config[division], rest);
 }
 
 void setup() {
@@ -63,11 +45,7 @@ void loop() {
 
   actuate();
   delay(dt);
-  _step = _step + 1;
-  if (_step == tsteps) {
-    _step = 0;
-
-  }
+  _step = (_step + 1) % tsteps;
 
 }
 
